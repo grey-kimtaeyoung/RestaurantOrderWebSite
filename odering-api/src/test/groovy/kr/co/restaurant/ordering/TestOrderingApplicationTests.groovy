1c6b939e14b372081d5b07e47b30d109e88c4a6e
grey-kimtaeyoung <growingpain89@gmail.com>
2019-12-26 22:31:56 +0900

    Make Restaurant spock test

--- odering-api/src/test/groovy/kr/co/restaurant/ordering/TestOrderingApplicationTests.groovy
+++ odering-api/src/test/groovy/kr/co/restaurant/ordering/TestOrderingApplicationTests.groovy
@@ -1,23 +1,8 @@
-package kr.co.restaurant.ordering;
+package kr.co.restaurant.ordering
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification;
 
 @SpringBootTest
 class TestOrderingApplicationTests extends Specification{
-
-    def "Spock 테스트 입니다"(){
-        // given: 테스트 하기 위한 기본 설정작업
-        given:
-        List<Integer> list = new ArrayList<>()
-
-        // 테스트할 대상 코드를 실행
-        when:
-        list.add(2)
-
-        // 테스트 결과 검증
-        then:
-        2==list.get(0)
-    }
 }
--- odering-api/src/test/groovy/kr/co/restaurant/ordering/domain/RestaurantTests.groovy
+++ odering-api/src/test/groovy/kr/co/restaurant/ordering/domain/RestaurantTests.groovy
@@ -3,21 +3,20 @@ package kr.co.restaurant.ordering.domain
 import spock.lang.Specification
 
 class RestaurantTests extends Specification {
-    def "temporary test"() {
-        given:
-        List<Integer> list = new ArrayList<>()
-        when:
-        list.add(1)
-        then:
-        2 == list.get(0)
-    }
+    def "레스토랑 도메인 초기셋팅시 정상작동여부 확인"(){
+        // 초기 변수 작업
+        setup:
+        def name = "Bob zip"
+        def address = "Seoul"
 
-    def "temporary second test"() {
-        given:
-        List<Integer> list = new ArrayList<>()
+        // 테스트할 대상 코드를 실행
         when:
-        list.add(1)
+        Restaurant restaurant = new Restaurant(name, address)
+
+        // 테스트 결과 검증
         then:
-        1 == list.get(0)
+        "Bob zip in Seoul" == restaurant.getInformation()
+        "Bob zip" == restaurant.getName()
+        "Seoul" == restaurant.getAddress()
     }
 }
