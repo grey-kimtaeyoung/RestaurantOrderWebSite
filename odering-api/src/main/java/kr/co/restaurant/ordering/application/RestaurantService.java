af1fcf4861915b6a405592d1290b51f4c4b6d666
grey-kimtaeyoung <growingpain89@gmail.com>
2019-12-31 19:52:43 +0900

    Add POST method test

--- odering-api/src/main/java/kr/co/restaurant/ordering/application/RestaurantService.java
+++ odering-api/src/main/java/kr/co/restaurant/ordering/application/RestaurantService.java
@@ -30,4 +30,7 @@ public class RestaurantService {
 
     return restaurant;
   }
+
+  public void addRestaurant(Restaurant restaurant) {
+  }
 }
--- odering-api/src/main/java/kr/co/restaurant/ordering/interfaces/RestaurantController.java
+++ odering-api/src/main/java/kr/co/restaurant/ordering/interfaces/RestaurantController.java
@@ -1,10 +1,15 @@
 package kr.co.restaurant.ordering.interfaces;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import kr.co.restaurant.ordering.application.RestaurantService;
 import kr.co.restaurant.ordering.domain.Restaurant;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +32,18 @@ public class RestaurantController {
       @PathVariable("id") Long id
   ) {
     return restaurantService.getRestaurant(id);
+  }
+
+  @PostMapping("/restaurants")
+  public ResponseEntity<?> create(@RequestBody Restaurant resource) throws URISyntaxException {
+    String name = "BeRyong";
+    String address = "Busan";
+
+    Restaurant restaurant = new Restaurant(1234L, name, address);
+    restaurantService.addRestaurant(restaurant);
+
+    URI location = new URI("/restaurants/" + restaurant.getId());
+    return ResponseEntity.created(location).body("{}");
   }
 
 }
